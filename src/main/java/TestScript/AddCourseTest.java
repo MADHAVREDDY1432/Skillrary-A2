7d500648762c35d76f107dabdf42dff0bb43a39a
MADHAVREDDY1432 <134061522+MADHAVREDDY1432@users.noreply.github.com>
2024-02-03 13:51:48 +0530

    Update AddCourseTest.java

--- src/main/java/TestScript/AddCourseTest.java
+++ src/main/java/TestScript/AddCourseTest.java
@@ -7,7 +7,7 @@ import org.testng.asserts.SoftAssert;
 
 import genericUtilities.BaseClass;
 import genericUtilities.IConstantpath;
-
+// this class verify if user is able to add courses
 public class AddCourseTest extends BaseClass  {
 
 
